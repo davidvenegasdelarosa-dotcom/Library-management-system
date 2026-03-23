4219358f3aa788de46ae2ef4e5310edfa0167a5f
veneegaa <david.venegasdelarosa@alum.uca.es>
2026-03-23 22:07:35 +0100

    libro terminado

--- src/main/java/com/example/demo/model/Libro.java
+++ src/main/java/com/example/demo/model/Libro.java
@@ -1,19 +1,29 @@
 package com.example.demo.model;
 import jakarta.persistence.*;
 
+@Entity //convertimos la clase en una tabla de base de datos
 public class Libro{
+
+    @Id //La variable "id" es la que distingue un libro de otro
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //si el ultimo libro tenia un id igual a 5, el siguiente sera con el id igual a 6
     private Long id;
     private String titulo;
+    private String autor;
     public Libro(){};
-    public Libro(Long id_, String titulo_){ //Constructor de un libro
+    public Libro(Long id_, String titulo_, String autor_){ //Constructor de un libro
         this.id=id_;
         this.titulo=titulo_;
-    }
-    public String mostrar_titulo(){
-        return(this.titulo);
-    }
-    public String mostrar_id(){
-        return(this.id);
+        this.autor=autor_;
     }
     
+	public Long getId() {
+		return id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+
 }
