76c195e9285a86117da694a15f154b4226333021
veneegaa <david.venegasdelarosa@alum.uca.es>
2026-03-26 22:24:04 +0100

    solucionado error de titulo y autor vacios

--- src/main/java/com/example/demo/model/Libro.java
+++ src/main/java/com/example/demo/model/Libro.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity //convertimos la clase en una tabla de base de datos
 public class Libro{
@@ -7,7 +8,9 @@ public class Libro{
     @Id //La variable "id" es la que distingue un libro de otro
     @GeneratedValue(strategy = GenerationType.IDENTITY) //si el ultimo libro tenia un id igual a 5, el siguiente sera con el id igual a 6
     private Long id;
+    @NotBlank(message = "El autor no puede estar vacío")
     private String titulo;
+    @NotBlank(message = "El autor no puede estar vacío")
     private String autor;
     public Libro(){};
     public void Libro_sinId(){};
